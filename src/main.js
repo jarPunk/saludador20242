d6a635ab8e9b5550ca83bb4eee538fcdd9963c90
jarPunk <jar_ax@hotmail.com>
2024-08-26 16:04:43 -0400

    Saludo personalizado con nombre y género

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 document.getElementById("saludar-btn").addEventListener("click", function() {
     const nombre = document.getElementById("nombre").value;
-    document.getElementById("saludo").innerText = "Hola, " + nombre;
+    const genero = document.getElementById("genero").value;
+    document.getElementById("saludo").innerText = "Hola, " + genero + " " + nombre;
 });

cc12beaeedf8eaeaa30cebe01357c52c6e345741
jarPunk <jar_ax@hotmail.com>
2024-08-26 16:12:49 -0400

    Implementación del saludo en inglés

--- src/main.js
+++ src/main.js
@@ -2,12 +2,23 @@ document.getElementById("saludar-btn").addEventListener("click", function() {
     const nombre = document.getElementById("nombre").value;
     const genero = document.getElementById("genero").value;
     const edad = document.getElementById("edad").value;
-    let saludo = "Hola, " + genero + " " + nombre;
-    
-    if (edad >= 18) {
-        saludo += ", es un placer saludarlo.";
-    } else {
-        saludo += ", ¡qué gusto verte!";
+    const idioma = document.getElementById("idioma").value;
+    let saludo;
+
+    if (idioma === "es") {
+        saludo = "Hola, " + genero + " " + nombre;
+        if (edad >= 18) {
+            saludo += ", es un placer saludarlo.";
+        } else {
+            saludo += ", ¡qué gusto verte!";
+        }
+    } else if (idioma === "en") {
+        saludo = "Hello, " + genero + " " + nombre;
+        if (edad >= 18) {
+            saludo += ", it's a pleasure to greet you.";
+        } else {
+            saludo += ", nice to see you!";
+        }
     }
 
     document.getElementById("saludo").innerText = saludo;
